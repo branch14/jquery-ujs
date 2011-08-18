96a1fc38b12205226386a3f2f91c143914876c43
phil <phil@branch14.org>
2011-08-18 11:54:24 +0200

    ujs works with image maps

--- src/rails.js
+++ src/rails.js
@@ -49,10 +49,11 @@
 
   $.rails = rails = {
     // Link elements bound by jquery-ujs
-    linkClickSelector: 'a[data-confirm], a[data-method], a[data-remote]',
+    linkClickSelector: 'a[data-confirm], a[data-method], a[data-remote], ' +
+                       'area[data-confirm], area[data-method], area[data-remote]',
 
-		// Select elements bound by jquery-ujs
-		selectChangeSelector: 'select[data-remote]',
+    // Select elements bound by jquery-ujs
+    selectChangeSelector: 'select[data-remote]',
 
     // Form elements bound by jquery-ujs
     formSubmitSelector: 'form',
@@ -270,13 +271,13 @@
     }
   });
 
-	$(rails.selectChangeSelector).live('change.rails', function(e) {
+  $(rails.selectChangeSelector).live('change.rails', function(e) {
     var link = $(this);
     if (!rails.allowAction(link)) return rails.stopEverything(e);
 
     rails.handleRemote(link);
     return false;
-  });	
+  });
 
   $(rails.formSubmitSelector).live('submit.rails', function(e) {
     var form = $(this),
